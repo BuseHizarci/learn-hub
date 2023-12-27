d7b27d7e9e85507fc9bbf1c382ed97be0351adcb
Buse Hizarci <buse.hzrci@gmail.com>
2023-12-27 12:22:49 +0000

    description slogan added

---
+++ .history/src/helpers/config_20231227121231.js
@@ -0,0 +1,28 @@
+export const config = {
+  project: {
+    name: "LearnHub",
+    slogan: "📢 Learn something new every day",
+    version: "1.0.0",
+    description: "Learn something new every day",
+    author: "Learn Hub Team",
+    copyright: "Copyright 2024. All rights reserved.",
+    theme: "light",
+  },
+  contact: {
+    email: "learnhub@gmail.edu",
+    phone: "123-456-7890",
+    address: "123 Main St, Anytown, USA 12345",
+    facebook: "https://www.facebook.com/",
+    twitter: "https://twitter.com/",
+    instagram: "https://www.instagram.com/",
+    youtube: "https://www.youtube.com/",
+  },
+};
+
+export const config = {
+  project: {
+      name: 'LearnHub',
+      slogan: "📢 LearnHub: Navigating Education, Empowering Futures!",
+      description: "Welcome to LearnHub, where innovation meets education! Our cutting-edge school management app is designed to seamlessly connect students, teachers, and parents in a collaborative learning ecosystem. With LearnHub, stay effortlessly organized with intuitive features for attendance tracking, grade management, and communication tools. Empower your educational journey with real-time insights, interactive learning resources, and streamlined administration. Experience the future of education management – LearnHub, transforming the way we learn, teach, and succeed together.",
+  }
+}
